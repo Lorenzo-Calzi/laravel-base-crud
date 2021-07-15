dfb075882ae3060e0eb62df323d1abf4562ba806
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-15 15:02:00 +0200

    Corretto design show page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,14 +1,10 @@
-@extends('layout.app')
+{{-- @extends('layout.app')
 
 @section('title', 'Comic')
 
 @section('main_content')
 
     <div class="comics">
-
-        {{-- <div class="button">
-            <a href="{{route('comics.index')}}">Home</a>
-        </div> --}}
 
         <h2>Comic</h2>
 
@@ -57,6 +53,81 @@
 
         <button type="submit" class="btn btn-danger">Delete</button>
     </form>
+@endsection --}}
+
+
+
+
+@extends('layout.app')
+
+@section('title', 'Comic page')
+
+@section('main_content')
+
+        <div class="comic">
+            <div class="bg_blue">
+                <div class="container">
+                    <img src="{{$comic->image}}" alt="">
+                </div>
+            </div>
+
+            <!-- Description -->
+            <div class="container">
+                <div class="desc">
+                    <div class="left_desc">
+                        <h2>{{$comic->name}}</h2>
+
+                        <div class="availability">
+                            <div>
+                                <span>U.S. Price: <strong>${{$comic->price}}</strong></span>
+                            </div>
+                            <div>
+                                <span class="available">Available</span>
+                                <strong>{{$comic->available}}</strong>
+                            </div>  
+                        </div>
+                        
+                        <p>{{$comic->description}}</p>
+                    </div>
+
+                    <div class="adv">
+                        <span>ADVERTISEMENT</span>
+                        <img src="{{asset('img/adv.jpg')}}" alt=""">
+                    </div>
+                </div>
+            </div> 
+            <!-- /Description -->
+
+            <!-- Infos -->
+            <div class="bg_white">
+                <div class="container">
+                    <div class="infos">
+                        <div class="info" style="border-left: 1px solid rgba(196, 196, 196, 1);">
+                            <span>digital comics</span>
+                            <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+                        </div>
+
+                        <div class="info">
+                            <span>shop dc</span>
+                            <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+                        </div>
+
+                        <div class="info">
+                            <span>comic shop locator</span>
+                            <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">  
+                        </div>
+
+                        <div class="info">
+                            <span>subscriptions</span>
+                            <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /Infos -->
+
+        </div>
+
 @endsection
 
 
