c3b2efe75059beda414aa2e5cf5b5ffa8f01a4d9
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-15 14:25:47 +0200

    Aggiunti header e footer

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header id="site_header">
+{{-- <header id="site_header">
     <div class="bg_blue"></div>
 
     <nav>
@@ -13,7 +13,44 @@
         </ul>
     </nav>
 
-    <!-- Jumbotron -->
     <div class="jumbo"></div>
-    <!-- /Jumbotron -->
-</header>
+    
+</header> --}}
+
+
+
+
+<header id="site_header">
+    <div class="up_header">
+            <div class="up_content container">
+                <span>DC POWER VISA <i class="far fa-registered"></i></span>
+                <span>ADDITIONAL DC SITES <i class="fas fa-caret-down"></i></span>
+            </div>
+        </div>
+    
+        <div class="bottom_header container">
+            <a href="{{route('home')}}">
+                <img src="{{asset('img/dc-logo.png')}}" alt="">
+            </a>
+            <div>
+                <ul>
+                    <li>
+                        <a href="{{route('comics.index')}}">HOME</a>
+                    </li>
+                    <li>
+                        <a href="{{route('comics.create')}}">CREATE</a>
+                    </li>
+                </ul>
+            </div>
+    
+            <div>
+                <input type="text" placeholder="Search"> 
+                <i class="fas fa-search"></i>
+            </div>
+        </div>
+    
+        <!-- Jumbotron -->
+        <div class="jumbo"></div>
+        <!-- /Jumbotron -->
+        
+    </header>
